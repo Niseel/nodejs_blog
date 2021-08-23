f2642df32f6b3d9092317ce025342f965e38eb04
Niseel <nguyencongthanh200199@gmail.com>
2021-08-23 18:06:20 +0700

    CRUD: Count documents soft delete

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,11 +4,24 @@ const { mongoosesToObject, mongooseToObject } = require('../../util/mongoose')
 class MeController {
     // [GET] /me/stored/blog
     storedBlog(req, res, next) {
-        Post.find({})
-            .then(posts => res.render('me/stored-blog', {
-                posts: mongoosesToObject(posts),
+        Promise.all([Post.find({}), Post.countDocumentsDeleted()])
+            .then(([posts, countDeleted]) => res.render('me/stored-blog', {
+                countDeleted,
+                posts: mongoosesToObject(posts), 
             }))
             .catch(next);
+
+        // Post.countDocumentsDeleted()
+        //     .then((amount) => {
+        //         console.log('Soluong: ', amount);
+        //     })
+        //     .catch(() =>{});
+            
+        // Post.find({})
+        //     .then(posts => res.render('me/stored-blog', {
+        //         posts: mongoosesToObject(posts),
+        //     }))
+        //     .catch(next);
     }
 
     // [GET] /me/trash/blog
